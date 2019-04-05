762c40a20661cf2edb7e3e4006e4a9984d7ee430
PaulloClara <paulloclara@gmail.com>
2019-04-05 10:29:22 -0300

    Configurando morgan, dotenv, rotas e porta

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Ola mundo')
+require('dotenv').config()
+
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+
+require('./routes')(app)
+
+const port = process.env.PORT || 8080
+
+app.listen(port)
