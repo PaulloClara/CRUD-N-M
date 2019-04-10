ef89b35a1b993fbbb5a660603b9c72aa99cf7efd
PaulloClara <paulloclara@gmail.com>
2019-04-10 11:48:27 -0300

    Add middleware de autenticação e reformulando algumas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,19 @@
 const router = require('express').Router()
+const authMiddleware = require('./middlewares/auth')
 
-const userController = require('./controllers/user')
-const othersController = require('./controllers/others')
+const userAccount = require('./controllers/userAccount')
+const usersControl = require('./controllers/usersControl')
+const others = require('./controllers/others')
 
-router.get('/', othersController.root)
-router.get('/all/user', userController.all)
+router.get('/', authMiddleware, others.root)
 
-router.get('/user/:username', userController.data)
-router.post('/user', userController.create)
-router.put('/user/:username', userController.update)
-router.delete('/user/:username', userController.remove)
+router.get('/all/user', usersControl.allUser)
+router.post('/login', usersControl.login)
+router.post('/register', usersControl.register)
+
+router.get('/user/', authMiddleware, userAccount.data)
+router.put('/user/update', authMiddleware, userAccount.update)
+router.put('/user/update/password', authMiddleware, userAccount.updatePassword)
+router.delete('/user/delete', authMiddleware, userAccount.remove)
 
 module.exports = app => app.use(router)
