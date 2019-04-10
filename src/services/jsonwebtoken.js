ec472751ff7a29488cb19e9baaab908120d50e2f
PaulloClara <paulloclara@gmail.com>
2019-04-10 11:39:25 -0300

    Init do modulo de tokenização (jsonwebtoken)

---
+++ src/services/jsonwebtoken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = {
+  sign: id => jwt.sign(
+    { id },
+    process.env.PRIVATE_KEY,
+    { expiresIn: 86400 }
+  ),
+  verify: token => jwt.verify(
+    token.split(' ')[1],
+    process.env.PRIVATE_KEY,
+    (err, decoded) => err ? false : decoded
+  )
+}
