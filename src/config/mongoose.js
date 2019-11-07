35900e57faee62b070b9139b6f17c1dc1097c242
paulloclara <paulloclara@gmail.com>
2019-11-06 21:16:08 -0300

    fix: resolve mongoose deprecation "Server Discovery and Monitoring engine"

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL, {
   useCreateIndex: true,
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 mongoose.Promise = global.Promise;
 
