1f90f6230c0c5188c1b1dc771e2992fcc9c00a22
paulloclara <paulloclara@gmail.com>
2020-03-08 12:23:44 -0300

    fix: convet salt rounds and validity env vars to number

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 
 module.exports = {
-  hash: password => bcrypt.hash(password, process.env.BCRYPT_SALT_ROUNDS),
+  hash: password =>
+    bcrypt.hash(password, Number(process.env.BCRYPT_SALT_ROUNDS)),
   compare: (password, hash) => bcrypt.compare(password, hash)
 };
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   sign: id =>
     jwt.sign({ id }, process.env.JWT_PRIVATE_KEY, {
-      expiresIn: process.env.JWT_VALIDITY
+      expiresIn: Number(process.env.JWT_VALIDITY)
     }),
   verify: token =>
     jwt.verify(token, process.env.JWT_PRIVATE_KEY, (error, result) =>
